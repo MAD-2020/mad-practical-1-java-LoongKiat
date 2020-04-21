6e16ee04ee4174e293209685e549d9d100014b86
LoongKiat <63993601+LoongKiat@users.noreply.github.com>
2020-04-21 21:26:08 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -4,20 +4,15 @@ public class Question4
 {
   public static void main(String[] args)
   {
-    /**
-     * Prints a right angle triangle with *. The base of the triangle depends on the integer given.
-     *    e.g.
-     *     > 5
-     *     *****
-     *     ****
-     *     ***
-     *     **
-     *     *
-     * 
-     * Hint: 2 loops are required. System.out.print(...) to print on single line.
-     */
-     
     Scanner in = new Scanner(System.in);
-    
+    System.out.println("Please enter an integar: ");
+    int num = in.nextInt();
+    while(num != 0){
+      for(int i = 0; i < num ; i++){
+        System.out.print("*");
+      }
+      System.out.println();
+      num -= 1;
+    }
   }
 }
