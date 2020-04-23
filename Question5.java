3c62141ec3ed8e1c40aed3b84e95d6023d31d021
LoongKiat <63993601+LoongKiat@users.noreply.github.com>
2020-04-23 20:53:58 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,32 +1,42 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Question5
 {
   public static void main(String[] args)
   {
-    /**
-     * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
-     * Print out the mode (highest occurrence) from the set of integers. 
-     *    e.g.
-     *     > 5
-     *     > 2
-     *     > 4
-     *     > 1
-     *     > 3
-     *     > 4
-     *     4
-     * 
-     *    e.g.
-     *     > 4
-     *     > 2
-     *     > 2
-     *     > 3
-     *     > 3
-     *     2
-     * Hint: Use a loop to get input. Use another 2 loops to find the mode
-     */
-     
     Scanner in = new Scanner(System.in);
-    
+    System.out.print("Please enter an integar: ");
+    int num = in.nextInt();
+    int no; 
+    int[] noArray = new int[num];
+    for (int i = 0; i < num; i++){
+      System.out.print("Please enter an integar: ");
+      no = in.nextInt();
+      noArray[i] = no;
+    }
+    int maxCount = 0;
+    int freqNo = noArray[0];
+    int count = 0;
+    Arrays.sort(noArray);
+    int f = noArray.length;
+    for(int i = 1; i < noArray.length; i++){
+      if(noArray[i] == noArray[i - 1]){
+        count ++;
+      }
+      else{
+        if(count > maxCount){
+          maxCount = count;
+          freqNo = noArray[i-1];
+        }
+        count = 0;
+      }
+    }
+    if (count > maxCount) 
+    { 
+      maxCount = count; 
+      freqNo = noArray[f - 1]; 
+    } 
+  System.out.println("The most frequent number is: " + freqNo);
   }
 }
