5ea6c132ced07991bd248b5be8255a70cc1eb423
LoongKiat <63993601+LoongKiat@users.noreply.github.com>
2020-04-24 12:06:15 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -5,9 +5,9 @@ public class Question3
   public static void main(String[] args)
   {
     Scanner in = new Scanner(System.in);
-    System.out.println("Please enter an integar: ");
+    //System.out.println("Please enter an integar: ");
     int num = in.nextInt();
     num = num * num;
-    System.out.println("New integar: " + num);
+    System.out.println(num);
   }
 }
