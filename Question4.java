5b0461d087a54edfb127da2d0322a0de48de8388
LoongKiat <63993601+LoongKiat@users.noreply.github.com>
2020-04-24 12:06:52 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -5,7 +5,7 @@ public class Question4
   public static void main(String[] args)
   {
     Scanner in = new Scanner(System.in);
-    System.out.println("Please enter an integar: ");
+    //System.out.println("Please enter an integar: ");
     int num = in.nextInt();
     while(num != 0){
       for(int i = 0; i < num ; i++){
